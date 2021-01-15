a38857485fd641b3d14a2add798943d37444a1c3
Michał Sieczkowski <michal@ethworks.io>
2021-01-15 20:55:53 +0100

    Add election districts loading to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src import config
+from src.ElectionDistrictsLoader import ElectionDistrictsLoader
 from src.InterpellationLoader import InterpellationLoader
 from src.VoivodeshipLoader import VoivodeshipLoader
 from src.CountyLoader import CountyLoader
@@ -13,6 +14,13 @@ def main_func():
         database_path=config.database['path'],
         parsed_interpellation_file=config.interpellations['file_path'])
     loader.load_to_database()
+
+    # load election districts
+    districts_loader = ElectionDistrictsLoader(database_file=config.database['path'],
+                                               districts_file_path=config.election_districts['path'])
+    districts_loader.load()
+    districts_loader.save_to_database()
+    county_to_district = districts_loader.get_county_to_district_map()
 
     # load county and voivodeship info from Teryt
     voivodeship_parser = VoivodeshipLoader(database_file=config.database['path'],
