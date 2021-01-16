02fd54a603fea1f3ef66d0e1f8a6bb16ecd53c51
Michał Sieczkowski <michal@ethworks.io>
2021-01-16 18:21:18 +0100

    Use relative paths in config.py

--- src/config.py
+++ src/config.py
@@ -1,32 +1,36 @@
 import logging
+from os import path
+
+current_file_dir = path.dirname(path.realpath(__file__))
+root_dir = path.abspath(path.join(current_file_dir, ".."))
 
 database = {
-    'path': '/home/krzysztof/PycharmProjects/musarum-res-publicae/data/senatores.db'
+    'path': path.join(root_dir, 'data/senatores.db')
 }
 interpellations = {
-    'file_path': '/home/krzysztof/PycharmProjects/musarum-res-publicae/data/interpelacje-processed.csv'
+    'file_path': path.join(root_dir, 'data/interpelacje-processed.csv')
 }
 logs = {
     'level': logging.DEBUG
 }
 
 voivodeship = {
-    'path': '/home/krzysztof/PycharmProjects/musarum-res-publicae/data/wojewodztwa.csv'
+    'path': path.join(root_dir, 'data/wojewodztwa.csv')
 }
 
 county = {
-    'path': '/home/krzysztof/PycharmProjects/musarum-res-publicae/data/powiaty.csv'
+    'path': path.join(root_dir, 'data/powiaty.csv')
 }
 
 settles = {
-    'dir_path': '/Users/michal/STUDIA/TASS/projekt 2/musarum-res-publica/data/settles',
+    'dir_path': path.join(root_dir, 'data/settles'),
     'files': ['dolnoslaskie.csv', 'krakowskie.csv', 'kujawskie.csv', 'lubelskie.csv', 'lubuskie.csv', 'ludzkie.csv',
               'mazowieckie.csv', 'opolskie.csv', 'podkarpackie.csv', 'podlaskie.csv', 'pomorskie.csv', 'slaskie.csv',
               'swiatokrzyskie.csv', 'vorpommern.csv', 'warminskie.csv', 'wielkopanskie.csv']
 }
 
 election_districts = {
-    'path': '/home/krzysztof/PycharmProjects/musarum-res-publicae/data/okregi_wyborcze.csv'
+    'path': path.join(root_dir, 'data/okregi_wyborcze.csv')
 }
 
 
