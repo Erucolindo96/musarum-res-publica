26e58c34914f4840bb698266f52ef0ba38e453a0
Michał Sieczkowski <michal@ethworks.io>
2021-01-18 23:13:59 +0100

    Add new stats: -- How many interpellations a given deputy made? -- How many interpellations deputies made that mention their election district?

--- src/sql/statistics.sql
+++ src/sql/statistics.sql
@@ -16,14 +16,6 @@ group by settle_id
 order by interpellations_count desc
 
 
--- How many interpellations a given deputy made?
--- Deputy -- Interpellations count
-select d.name, count(d_i.interpellation_id) as interpellations_count from deputy_interpellation as d_i
-join deputy as d on d_i.deputy_id=d.id
-group by d.id
-order by interpellations_count desc
-
-
 -- List all mentions of districts in interpellations
 -- Deputy - Interpellation - Election district
 select d.name, i_s.interpellation_id, c.district_number
@@ -43,3 +35,38 @@ join county as c on c.id=s.county_id and c.voivodeship_id=s.voivodeship_id
 join deputy_interpellation as d_i on i_s.interpellation_id = d_i.interpellation_id
 join deputy as d on d_i.deputy_id=d.id
 group by i_s.interpellation_id, c.district_number
+
+
+-- How many interpellations a given deputy made?
+-- Deputy -- Interpellations count
+select d.name, count(d_i.interpellation_id) as interpellations_count from deputy_interpellation as d_i
+join deputy as d on d_i.deputy_id=d.id
+group by d.id
+order by interpellations_count desc
+
+
+-- How many interpellations deputies made that mention their election district?
+-- Deputy name - Interpellation ids that concern their region
+select d.name, i_s.interpellation_id
+from interpellation_settles as i_s
+join settle as s on i_s.settle_id=s.id
+join county as c on c.id=s.county_id and c.voivodeship_id=s.voivodeship_id
+join deputy_interpellation as d_i on i_s.interpellation_id = d_i.interpellation_id
+join deputy as d on d_i.deputy_id=d.id
+where c.district_number=d.district_number
+group by d.id, i_s.interpellation_id
+
+-- Deputy - Deputy district - Election district - Interpellations count
+select name, count(*) as interpellations_count from (
+    select d.name, i_s.interpellation_id
+    from interpellation_settles as i_s
+    join settle as s on i_s.settle_id=s.id
+    join county as c on c.id=s.county_id and c.voivodeship_id=s.voivodeship_id
+    join deputy_interpellation as d_i on i_s.interpellation_id = d_i.interpellation_id
+    join deputy as d on d_i.deputy_id=d.id
+    where c.district_number=d.district_number
+    group by d.id, i_s.interpellation_id
+)
+group by name
+order by interpellations_count desc
+
